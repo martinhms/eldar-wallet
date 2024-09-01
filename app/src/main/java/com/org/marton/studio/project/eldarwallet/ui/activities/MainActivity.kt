e0f76ee2edf8101b7c61591c43769e107e411682
martinhms <martinhms2015@gmail.com>
2024-09-01 12:27:43 -0300

    [LOGIN] - Adde Login activity - logo (#1)
    * [LOGIN] - Adde Login activity - logo
    
    * [LOGIN] - Init - LoginViewModel
    
    ---------
    
    Co-authored-by: Martin Horacio Mendez <martinhmendez@MBP-de-Martin.fibertel.com.ar>

--- app/src/main/java/com/org/marton/studio/project/eldarwallet/ui/activities/MainActivity.kt
+++ app/src/main/java/com/org/marton/studio/project/eldarwallet/ui/activities/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.org.marton.studio.project.eldarwallet
+package com.org.marton.studio.project.eldarwallet.ui.activities
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.org.marton.studio.project.eldarwallet.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/org/marton/studio/project/eldarwallet/ui/activities/login/LoginViewModel.kt
@@ -0,0 +1,5 @@
+package com.org.marton.studio.project.eldarwallet.ui.activities.login
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel()
