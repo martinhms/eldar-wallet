8d0a97041db856def5a1136a084bfd11d384c1c1
Martin Horacio Mendez <martinhmendez@MBP-de-Martin.fibertel.com.ar>
2024-09-02 21:15:34 -0300

    [ADD-CARD] - Add getUserDigitalCardsUseCase

--- app/src/main/java/com/org/marton/studio/project/eldarwallet/ui/activities/main/MainViewModel.kt
+++ app/src/main/java/com/org/marton/studio/project/eldarwallet/ui/activities/main/MainViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.org.marton.studio.project.eldarwallet.domain.usecase.GetUserDataUseCase
+import com.org.marton.studio.project.eldarwallet.domain.usecase.GetUserDigitalCardsUseCase
 import com.org.marton.studio.project.eldarwallet.ui.models.DigitalCard
 import com.org.marton.studio.project.eldarwallet.ui.models.UserData
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -18,6 +19,7 @@ import javax.inject.Inject
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val getUserDataUseCase: GetUserDataUseCase,
+    private val getUserDigitalCardsUseCase: GetUserDigitalCardsUseCase
 ) : ViewModel() {
 
     private val _userData = MutableLiveData<UserData>()
@@ -31,40 +33,27 @@ class MainViewModel @Inject constructor(
     fun getUserData(userId: String) {
         viewModelScope.launch(Dispatchers.IO) {
             getUserDataUseCase(userId).collect { userData ->
-                val digitalCards = getDigitalCards()
-                val balance = getBalance()
-                _userData.postValue(
-                    UserData(
-                        id = userData.id,
-                        userName = userData.userName,
-                        userLastname = userData.userLastname,
-                        password = userData.password,
-                        cards = digitalCards,
-                        balance = balance,
-                        identification = userData.identification,
-                        email = userData.email,
-                        avatar = userData.avatar,
-                        createdTime = userData.createdTime,
-                        updatedTime = userData.updatedTime,
+                getUserDigitalCardsUseCase(userId).collect { digitalCards ->
+                    val balance = getBalance()
+                    _userData.postValue(
+                        UserData(
+                            id = userData.id,
+                            userName = userData.userName,
+                            userLastname = userData.userLastname,
+                            password = userData.password,
+                            cards = digitalCards,
+                            balance = balance,
+                            identification = userData.identification,
+                            email = userData.email,
+                            avatar = userData.avatar,
+                            createdTime = userData.createdTime,
+                            updatedTime = userData.updatedTime,
+                        )
                     )
-                )
+                }
             }
         }
     }
+
+    private fun getBalance() = 1500.0
 }
-
-private fun getDigitalCards() = initCardList()
-
-private fun getBalance() = 1500.0
-
-fun initCardList() = listOf(
-    DigitalCard(
-        1234567890,
-        1234,
-        "Santander",
-        "Visa",
-        "Débito",
-        123,
-        1233,
-    )
-)
